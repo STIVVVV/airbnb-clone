b0899f9c48f4ea51f2d4e98f9a6fbff6a73cb9d8
STIVVVV <stevencanfield2000@gmail.com>
2022-05-25 12:41:22 -0400

    Refactored card and app code

--- src/components/Card/Card.js
+++ src/components/Card/Card.js
@@ -1,29 +1,28 @@
 import React from "react"
 
 const Card = (props) => {
-
     let badgeText
-    if(props.openSpots === 0 )
+    if(props.item.openSpots === 0 )
     {
         badgeText = "SOLD OUT"
     }
-    else if(props.location === "Online")
+    else if(props.item.location === "Online")
     {
         badgeText = "Online"
     }
-
+    
     return (
         <div className='card'>
             {badgeText && <div className="card--badge">{badgeText}</div>}
-            <img src = {`/images/${props.img}`} className="card--image" alt="card-header-i"/>
+            <img src = {`/images/${props.item.coverImg}`} className="card--image" alt="card-header-i"/>
             <div className="card--stats">
                 <img src = {"/images/Star.png"} className="card--star" alt="star Icon"/>
-                <span>{props.rating}</span>
-                <span className="gray">({props.reviewCount}) • </span>
-                <span className="gray">{props.location}</span>
+                <span>{props.item.rating}</span>
+                <span className="gray">({props.item.stats.rating}) • </span>
+                <span className="gray">{props.item.location}</span>
             </div>
-            <p className="card--title">{props.title}</p>
-            <p className="card--price"><span className="bold">From {props.price}</span> / person</p>
+            <p className="card--title">{props.item.title}</p>
+            <p className="card--price"><span className="bold">From {props.item.price}</span> / person</p>
         </div>
     ) 
 }
